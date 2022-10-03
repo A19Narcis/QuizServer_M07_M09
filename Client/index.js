22cabc14be973f1d363cd1233df2ddadaaf87ea0
A19Narcis <a19nargomcar@inspedralbes.cat>
2022-10-03 10:03:55 +0200

    Inici resultats

--- Client/index.js
+++ Client/index.js
@@ -3,11 +3,13 @@ var app = new Vue({
     data: {
         info: {num : 4},
         preguntes: [],
+        isVisible: false
     },
     vuetify: new Vuetify(),
     methods: {
         getQuest: function () {
             console.log("Get data");
+            this.isVisible = true;
             const myHeaders = new Headers();
         
             fetch("http://localhost:3000/getPreguntes",
@@ -34,6 +36,29 @@ var app = new Vue({
                     //console.log("Error: " + error)
                 }
             );
+        },
+
+        veureSelects: function() {
+            console.log("Getting selected options");
+
+            var ele = document.getElementsByName('radioBTN');
+
+            console.log(ele.length);
+            var selected = [];
+            
+            for (let i = 0; i < ele.length; i++) {
+                console.log(ele[i].value);
+                if (ele[i].checked) {
+                    console.log("Selected...");
+                    console.log("ADD -> " + ele[i].value);
+                    selected.push(ele[i].value);
+                    //document.getElementById("result").innerHTML = "Resposta: " + selected;
+                }
+            }
+
+            selected.forEach(function(entry){
+                console.log("- " + entry);
+            })
         }
     },
     
