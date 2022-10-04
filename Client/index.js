70a273bc41107c28afcc85f35d9c6521dda713ba
A19Narcis <a19nargomcar@inspedralbes.cat>
2022-10-04 13:17:52 +0200

    Errors >> NUM QUESTIONS

--- Client/index.js
+++ Client/index.js
@@ -7,6 +7,8 @@ var app = new Vue({
         radioGroup: new Array(this.num),
         solucions: 0,
         acabat: false,
+        snackbar: false,
+        text: "",
         start: true
     },
     vuetify: new Vuetify(),
@@ -16,7 +18,6 @@ var app = new Vue({
             this.isVisible = true;
             this.start = false;
             this.info.num = document.getElementById("numQuestions").value;
-            console.log(this.info.num);
             const myHeaders = new Headers();
         
             fetch("http://localhost:3000/getPreguntes",
@@ -46,37 +47,46 @@ var app = new Vue({
         },
 
         veureSelects: function() {
-            console.log("Get Resultats");
-            console.log(this.radioGroup);
-            const myHeaders = new Headers();
-            this.acabat = true;
-        
-            fetch("http://localhost:3000/finalista",
-                {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept' : 'application/json',
-                },
-                mode: "cors",
-                cache: "default",
-                body: JSON.stringify(this.radioGroup)
-                }
-            ).then(
-                (response)=>{
-                    return(response.json());
-                }
-            ).then(
-                (data) => {
-                    this.solucions = data;
-                    console.log(data);
-                    
-                }
-            ).catch(
-                (error) => {
-                    //console.log("Error: " + error)
-                }
-            );
+            if(this.radioGroup[0] == undefined){
+                this.radioGroup.length = 0;
+            }
+            if (this.radioGroup.length == this.info.num/*this.radioGroup.length == this.info.num*/) {
+                this.snackbar = true;
+                console.log("Get Resultats");
+                console.log(this.radioGroup);
+                const myHeaders = new Headers();
+                this.acabat = true;
+            
+                fetch("http://localhost:3000/finalista",
+                    {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept' : 'application/json',
+                    },
+                    mode: "cors",
+                    cache: "default",
+                    body: JSON.stringify(this.radioGroup)
+                    }
+                ).then(
+                    (response)=>{
+                        return(response.json());
+                    }
+                ).then(
+                    (data) => {
+                        this.solucions = data;
+                        console.log(data);
+                        this.text = "Resultat => " + this.solucions + "/" + this.info.num
+                        
+                    }
+                ).catch(
+                    (error) => {
+                        //console.log("Error: " + error)
+                    }
+                );           
+            } else {
+                console.log("No se puede");
+            }
         },
     },
 })
